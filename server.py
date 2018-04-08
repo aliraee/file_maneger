28ef6fd321bc5002e12562543c46997e49e06539
hosseiniust <37348959+hosseiniust@users.noreply.github.com>
2018-04-08 18:06:07 +0430

    Add files via upload

--- server.py
+++ server.py
@@ -1,44 +1,14 @@
 import socket
-import threading
-import  os
 
-def RetrFile(name , sock):
-    filename = sock.recv (1024)
-    if os.path.isfile(filename):
-        sock.send("EXISTS "+str(os.path.getsize(filename)))
-        userResponse = sock.recv(1024)
-        if userResponse[:2] =="OK":
-            with open (filename , 'rb') as f:
-                bytesToSend = f.read(1024)
-                sock.send(bytesToSend)
-                while bytesToSend != "":
-                    bytesToSend = f.read(1024)
-                    sock.send(bytesToSend)
+socketserver = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+socketserver.bind(('localhost',4444))
+socketserver.listen(2)
 
-        else:
-            sock.send("ERR")
-
-        sock.close()
-
-
-def main():
-    host = "127.0.0.1"
-    port = 5000
-
-    s= socket.socket()
-    s.bind((host , port))
-    s.listen(5)
-
-    print("server started ")
+while True:
+    connection,address = socketserver.accept()
     while True:
-        c , addr = s.accept()
-        print("client connected "+ str(addr))
-
-        t = threading.Thread(target= RetrFile , args= ("RetrThread" , c))
-        t.start()
-
-    s.close()
-
-
-if __name__ =="__main__":
-    main()
+        buf = connection.recv(1024).decode()
+        if len(buf) > 0:
+            print(buf)
+        
+        
