b24f406b1d06ae2be05b75a5cfbeeadac34a5618
hosseiniust <37348959+hosseiniust@users.noreply.github.com>
2018-04-09 18:47:30 +0430

    Add files via upload

--- client_chat.py
+++ client_chat.py
@@ -1,6 +1,23 @@
 import socket
+import threading
 
 clinet = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-clinet.connect(('localhost',4444))
-while True:
+clinet.connect(('172.17.8.37',4444))
+
+#functions
+def send_client_to_server():
     clinet.send(input().encode('UTF-8'))
+def recive_client_from_server():
+    mas1 = clinet.recv(1024).decode('UTF_8')
+    if len(mas1) != 0:
+        print(mas1)
+
+while True:
+    t1 = threading.Thread( target = send_client_to_server())
+    t2 = threading.Thread( target = recive_client_from_server())
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+    
--- server_chat.py
+++ server_chat.py
@@ -1,14 +1,32 @@
 import socket
+import threading
 
 socketserver = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-socketserver.bind(('localhost',4444))
+socketserver.bind(('172.17.8.37',4444))
 socketserver.listen(2)
+
+
+#functions
+def recive_server_from_client():
+    buf = connection.recv(1024).decode('UTF-8')
+    if len(buf) > 0:
+        print(buf)
+def send_to_client():
+    mas = input().encode('UTF-8')
+    connection.send(mas)
+
+
+
 
 while True:
     connection,address = socketserver.accept()
     while True:
-        buf = connection.recv(1024).decode('UTF-8')
-        if len(buf) > 0:
-            print(buf)
+        t1 = threading.Thread( target = recive_server_from_client())
+        t2 = threading.Thread( target = send_to_client())
+        t1.start()
+        t2.start()
+        #t1.join()
+        #t2.join()
+
         
         
